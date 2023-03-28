9bd2a5fbfdba839499a5b6fb1b518a003a13fb40
Andrew Kane <andrew@ankane.org>
2023-03-27 18:27:28 -0700

    Added example for EF Core

---
+++ tests/Pgvector.Tests/EntityFrameworkCoreTests.cs
@@ -0,0 +1,57 @@
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using Pgvector.Npgsql;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pgvector.Tests;
+
+public class ItemContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connString = "Host=localhost;Database=pgvector_dotnet_test";
+        optionsBuilder.UseNpgsql(connString).UseSnakeCaseNamingConvention();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("vector");
+
+        // TODO set lists = 1
+        // modelBuilder.Entity<Item>()
+        //     .HasIndex(i => i.Embedding)
+        //     .HasMethod("ivfflat")
+        //     .HasOperators("vector_l2_ops");
+    }
+}
+
+[Table("efcore_items")]
+public class Item
+{
+    public int Id { get; set; }
+    [Column(TypeName = "vector(3)")]
+    public string Embedding { get; set; } = null!;
+}
+
+public class EntityFrameworkCoreTests
+{
+    [Fact]
+    public async Task Main()
+    {
+        await using var ctx = new ItemContext();
+        await ctx.Database.EnsureDeletedAsync();
+        await ctx.Database.EnsureCreatedAsync();
+
+        var embedding1 = new Vector(new float[] { 1, 1, 1 });
+        var embedding2 = new Vector(new float[] { 2, 2, 2 });
+        var embedding3 = new Vector(new float[] { 1, 1, 2 });
+        ctx.Database.ExecuteSql($"INSERT INTO efcore_items (embedding) VALUES ({embedding1.ToString()}::vector), ({embedding2.ToString()}::vector), ({embedding3.ToString()}::vector)");
+
+        var embedding = new Vector(new float[] { 1, 1, 1 });
+        var items = await ctx.Items.FromSql($"SELECT id, embedding::text FROM efcore_items ORDER BY embedding <-> {embedding.ToString()}::vector LIMIT 5").ToListAsync();
+        foreach (Item item in items)
+            Console.WriteLine(new Vector(item.Embedding));
+    }
+}
