70a731b45f95bd14d5572e8806afb8c392d26bc4
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2023-04-24 00:33:58 -0600

    converted to netstandard2.0

--- src/Pgvector/Npgsql/VectorTypeHandlerResolver.cs
+++ src/Pgvector/Npgsql/VectorTypeHandlerResolver.cs
@@ -2,56 +2,81 @@ using Npgsql.Internal;
 using Npgsql.Internal.TypeHandling;
 using Npgsql.PostgresTypes;
 using NpgsqlTypes;
+using System;
 
-namespace Pgvector.Npgsql;
-
-public class VectorTypeHandlerResolver : TypeHandlerResolver
+namespace Pgvector.Npgsql
 {
-    readonly NpgsqlDatabaseInfo _databaseInfo;
-    readonly VectorHandler? _vectorHandler;
+	public class VectorTypeHandlerResolver : TypeHandlerResolver
+	{
+		readonly NpgsqlDatabaseInfo _databaseInfo;
+		readonly VectorHandler _vectorHandler;
 
-    internal VectorTypeHandlerResolver(NpgsqlConnector connector)
-    {
-        _databaseInfo = connector.DatabaseInfo;
+		internal VectorTypeHandlerResolver(NpgsqlConnector connector)
+		{
+			_databaseInfo = connector.DatabaseInfo;
 
-        var pgVectorType = PgType("vector");
-        if (pgVectorType is not null)
-        {
-            _vectorHandler = new VectorHandler(pgVectorType);
-        }
-    }
+			var pgVectorType = PgType("vector");
 
-    public override NpgsqlTypeHandler? ResolveByDataTypeName(string typeName)
-        => typeName switch
-        {
-            "vector" => _vectorHandler,
-            _ => null
-        };
+			if (!(pgVectorType is null))
+			{
+				_vectorHandler = new VectorHandler(pgVectorType);
+			}
+		}
 
-    public override NpgsqlTypeHandler? ResolveByClrType(Type type)
-        => ClrTypeToDataTypeName(type) is { } dataTypeName && ResolveByDataTypeName(dataTypeName) is { } handler
-            ? handler
-            : null;
+		public override NpgsqlTypeHandler ResolveByDataTypeName(string typeName)
+		{
+			switch (typeName)
+			{
+				case "vector":
+					return _vectorHandler;
+				default:
+					return null;
+			};
+		}
 
-    public override TypeMappingInfo? GetMappingByDataTypeName(string dataTypeName)
-        => DoGetMappingByDataTypeName(dataTypeName);
+		public override NpgsqlTypeHandler ResolveByClrType(Type type)
+		{
+			var dataTypeName = ClrTypeToDataTypeName(type);
 
-    internal static string? ClrTypeToDataTypeName(Type type)
-    {
-        if (type == typeof(Vector))
-        {
-            return "vector";
-        }
+			if (dataTypeName != null)
+			{
+				var handler = ResolveByDataTypeName(dataTypeName);
 
-        return null;
-    }
+				if (handler != null)
+				{
+					return handler;
+				}
+			}
 
-    internal static TypeMappingInfo? DoGetMappingByDataTypeName(string dataTypeName)
-        => dataTypeName switch
-        {
-            "vector" => new(NpgsqlDbType.Unknown, "vector"),
-            _ => null
-        };
+			return null;
+		}
 
-    PostgresType? PgType(string pgTypeName) => _databaseInfo.TryGetPostgresTypeByName(pgTypeName, out var pgType) ? pgType : null;
+		public override TypeMappingInfo GetMappingByDataTypeName(string dataTypeName)
+		{
+			return DoGetMappingByDataTypeName(dataTypeName);
+		}
+
+		internal static string ClrTypeToDataTypeName(Type type)
+		{
+			if (type == typeof(Vector))
+			{
+				return "vector";
+			}
+
+			return null;
+		}
+
+		internal static TypeMappingInfo DoGetMappingByDataTypeName(string dataTypeName)
+		{ 
+			switch(dataTypeName)
+			{
+				case "vector":
+					return new TypeMappingInfo(NpgsqlDbType.Unknown, "vector");
+				default:
+					return null;
+			}
+		}
+
+		PostgresType PgType(string pgTypeName) => _databaseInfo.TryGetPostgresTypeByName(pgTypeName, out var pgType) ? pgType : null;
+	}
 }
--- src/Pgvector/Vector.cs
+++ src/Pgvector/Vector.cs
@@ -1,26 +1,29 @@
-namespace Pgvector;
+using System;
 
-public class Vector
+namespace Pgvector
 {
-    private float[] vec;
+	public class Vector
+	{
+		private float[] vec;
 
-    public Vector(float[] v)
-    {
-        vec = v;
-    }
+		public Vector(float[] v)
+		{
+			vec = v;
+		}
 
-    public Vector(String s)
-    {
-        vec = Array.ConvertAll(s.Substring(1, s.Length - 2).Split(","), v => float.Parse(v));
-    }
+		public Vector(string s)
+		{
+			vec = Array.ConvertAll(s.Substring(1, s.Length - 2).Split(','), v => float.Parse(v));
+		}
 
-    public override string ToString()
-    {
-        return String.Concat("[", String.Join(",", vec), "]");
-    }
+		public override string ToString()
+		{
+			return string.Concat("[", string.Join(",", vec), "]");
+		}
 
-    public float[] ToArray()
-    {
-        return vec;
-    }
+		public float[] ToArray()
+		{
+			return vec;
+		}
+	}
 }
