989911ae21b4c84b7e568738be686800cd1bdf7e
Alexander Gluschenko <qugluschenko@gmail.com>
2023-08-27 17:11:13 +0600

    [test] Add tests for Vector functions

--- tests/Pgvector.Tests/EntityFrameworkCoreTests.cs
+++ tests/Pgvector.Tests/EntityFrameworkCoreTests.cs
@@ -37,12 +37,18 @@ public class Item
 
 public class EntityFrameworkCoreTests
 {
+    private async Task<ItemContext> GetContextAsync()
+    {
+        var ctx = new ItemContext();
+        await ctx.Database.EnsureDeletedAsync();
+        await ctx.Database.EnsureCreatedAsync();
+        return ctx;
+    }
+
     [Fact]
     public async Task Main()
     {
-        await using var ctx = new ItemContext();
-        await ctx.Database.EnsureDeletedAsync();
-        await ctx.Database.EnsureCreatedAsync();
+        await using var ctx = await GetContextAsync();
 
         ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 1 }) });
         ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 2, 2, 2 }) });
@@ -50,7 +56,85 @@ public class EntityFrameworkCoreTests
         ctx.SaveChanges();
 
         var embedding = new Vector(new float[] { 1, 1, 1 });
-        var items = await ctx.Items.FromSql($"SELECT * FROM efcore_items ORDER BY embedding <-> {embedding} LIMIT 5").ToListAsync();
+        var items = await ctx.Items
+            .FromSql($"SELECT * FROM efcore_items ORDER BY embedding <-> {embedding} LIMIT 5")
+            .ToListAsync();
+
+        foreach (Item item in items)
+        {
+            if (item.Embedding != null)
+            {
+                Console.WriteLine(item.Embedding);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task EuclideanDistance()
+    {
+        await using var ctx = await GetContextAsync();
+
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 1 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 2, 2, 2 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 2 }) });
+        ctx.SaveChanges();
+
+        var embedding = new Vector(new float[] { 1, 1, 1 });
+        var items = await ctx.Items
+            .OrderBy(x => x.Embedding!.EuclideanDistance(embedding))
+            .Take(5)
+            .ToListAsync();
+
+        foreach (Item item in items)
+        {
+            if (item.Embedding != null)
+            {
+                Console.WriteLine(item.Embedding);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task CosineDistance()
+    {
+        await using var ctx = await GetContextAsync();
+
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 1 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 2, 2, 2 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 2 }) });
+        ctx.SaveChanges();
+
+        var embedding = new Vector(new float[] { 1, 1, 1 });
+        var items = await ctx.Items
+            .OrderBy(x => x.Embedding!.CosineDistance(embedding))
+            .Take(5)
+            .ToListAsync();
+
+        foreach (Item item in items)
+        {
+            if (item.Embedding != null)
+            {
+                Console.WriteLine(item.Embedding);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task InnerProduct()
+    {
+        await using var ctx = await GetContextAsync();
+
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 1 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 2, 2, 2 }) });
+        ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 2 }) });
+        ctx.SaveChanges();
+
+        var embedding = new Vector(new float[] { 1, 1, 1 });
+        var items = await ctx.Items
+            .OrderBy(x => x.Embedding!.InnerProduct(embedding))
+            .Take(5)
+            .ToListAsync();
+
         foreach (Item item in items)
         {
             if (item.Embedding != null)
