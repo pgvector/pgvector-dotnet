9271422accfc92fd5c28dd1477005a1e13e51b28
Shay Rojansky <roji@roji.org>
2023-10-13 22:42:52 +0200

    Define equality/GetHashCode for the Vector class

--- src/Pgvector/Vector.cs
+++ src/Pgvector/Vector.cs
@@ -4,22 +4,60 @@ using System.Linq;
 
 namespace Pgvector;
 
-public class Vector
+public class Vector : IEquatable<Vector>
 {
-    internal ReadOnlyMemory<float> vec;
+    public ReadOnlyMemory<float> Memory { get; }
 
     public Vector(ReadOnlyMemory<float> v)
-        => vec = v;
-
-    public Vector(float[] v)
-        => vec = new ReadOnlyMemory<float>(v);
+        => Memory = v;
 
     public Vector(string s)
         => new Vector(Array.ConvertAll(s.Substring(1, s.Length - 2).Split(','), v => float.Parse(v, CultureInfo.InvariantCulture)));
 
     public override string ToString()
-        => string.Concat("[", string.Join(",", vec.ToArray().Select(v => v.ToString(CultureInfo.InvariantCulture))), "]");
+        => string.Concat("[", string.Join(",", Memory.ToArray().Select(v => v.ToString(CultureInfo.InvariantCulture))), "]");
 
     public float[] ToArray()
-        => vec.ToArray();
+        => Memory.ToArray();
+
+    public bool Equals(Vector other)
+    {
+        if (other is null || other.Memory.Length != Memory.Length)
+        {
+            return false;
+        }
+
+        var span1 = Memory.Span;
+        var span2 = other.Memory.Span;
+
+        for (var i = 0; i < Memory.Length; i++)
+        {
+            if (span1[i] != span2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object? obj)
+        => obj is Vector vector && Equals(vector);
+
+    public static bool operator ==(Vector x, Vector y) => x.Equals(y);
+    public static bool operator !=(Vector x, Vector y) => !(x == y);
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        var span = Memory.Span;
+
+        for (var i = 0; i < span.Length; i++)
+        {
+            hashCode.Add(span[i]);
+        }
+
+        return hashCode.ToHashCode();
+    }
 }
