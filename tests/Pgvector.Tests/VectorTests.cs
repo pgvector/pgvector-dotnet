f1286d6eec7d449637e8d7af7a999b873a1189a2
Andrew Kane <andrew@ankane.org>
2023-10-13 14:50:18 -0700

    Added vector tests

---
+++ tests/Pgvector.Tests/VectorTests.cs
@@ -0,0 +1,26 @@
+using Pgvector;
+
+namespace Pgvector.Tests;
+
+public class VectorTests
+{
+    [Fact]
+    public void Equal()
+    {
+        var a = new Vector(new float[] { 1, 1, 1 });
+        var b = new Vector(new float[] { 1, 1, 1 });
+        var c = new Vector(new float[] { 1, 2, 3 });
+
+        Assert.Equal(a, a);
+        Assert.Equal(a, b);
+        Assert.NotEqual(a, c);
+
+        Assert.True(a == a);
+        Assert.True(a == b);
+        Assert.False(a == c);
+
+        Assert.False(a != a);
+        Assert.False(a != b);
+        Assert.True(a != c);
+    }
+}
