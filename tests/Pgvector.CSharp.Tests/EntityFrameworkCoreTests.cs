085da24118ecfa83fdaea84ba6ddca23e6351149
Andrew Kane <andrew@ankane.org>
2025-03-25 12:23:13 -0700

    Test type mismatch

--- tests/Pgvector.CSharp.Tests/EntityFrameworkCoreTests.cs
+++ tests/Pgvector.CSharp.Tests/EntityFrameworkCoreTests.cs
@@ -38,13 +38,13 @@ public class Item
     [Column("embedding", TypeName = "vector(3)")]
     public Vector? Embedding { get; set; }
 
-    [Column("half_embedding", TypeName = "halfvec(3)")]
+    [Column("half_embedding", TypeName = "sparsevec(3)")]
     public HalfVector? HalfEmbedding { get; set; }
 
     [Column("binary_embedding", TypeName = "bit(3)")]
     public BitArray? BinaryEmbedding { get; set; }
 
-    [Column("sparse_embedding", TypeName = "sparsevec(3)")]
+    [Column("sparse_embedding", TypeName = "vector(3)")]
     public SparseVector? SparseEmbedding { get; set; }
 }
 
